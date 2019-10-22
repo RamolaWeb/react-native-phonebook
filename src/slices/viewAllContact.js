f41eac507a497da4868e4988cf4c789623bb0311
humblenerd <humblecodernith@gmail.com>
2019-10-22 15:43:15 +0530

    Sorted Order

--- src/slices/viewAllContact.js
+++ src/slices/viewAllContact.js
@@ -42,7 +42,17 @@ const viewAllContactSlice = createSlice({
           ]
         }
       }
-      state.contacts = data
+      state.contacts = data.sort((a, b) => {
+        var nameA = a.title.toUpperCase()
+        var nameB = b.title.toUpperCase()
+        if (nameA < nameB) {
+          return -1
+        }
+        if (nameA > nameB) {
+          return 1
+        }
+        return 0
+      })
     }
   }
 })
